9f4d61ef3ca678dd089d5e27324bc178e6c8a4ce
Niki Amini-Naieni <niki@adainnovations.ai>
2024-01-23 13:43:14 +0000

    printing debugging for evaluation

--- engine.py
+++ engine.py
@@ -161,6 +161,9 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
     for samples, targets in metric_logger.log_every(data_loader, 10, header, logger=logger):
         samples = samples.to(device)
 
+        print("samples.shape: " + str(samples.shape))
+        print("targets.shape: " + str(targets.shape))
+
         targets = [{k: to_device(v, device) for k, v in t.items()} for t in targets]
 
         bs = samples.tensors.shape[0]
